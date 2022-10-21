36f3d58a7dd34b6eddcf5de94a5c60f45c97bed7
Vikas-jangid <vikas.jangid@infobeans.com>
2022-10-21 18:10:39 +0530

    configured tailwind css added navbar and setup routing for the components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import './index.css';
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import NotFound from "./components/notFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>E-commerce</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<Navigate replace to="/404" />} />
+      </Routes>
+    </BrowserRouter>  
+    
   );
 }
 
