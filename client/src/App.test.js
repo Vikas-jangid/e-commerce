a0f1b1fbb147bdfd25395e4e530754fe43cd39d1
Vikas-jangid <vikas.jangid@infobeans.com>
2022-11-14 11:29:29 +0530

    enzyme-adapter-react-18 is not working so added enzyme-adapter-react-16 and its working fine

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Enzyme, {shallow} from "enzyme";
-import { EnzymeAdapter } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
- Enzyme.configure({adapter: new EnzymeAdapter});
+ Enzyme.configure({adapter: new Adapter()});
 
  test('render app without breaking', () => {
+    const wrapper = shallow(<App />);
    
  });
 
