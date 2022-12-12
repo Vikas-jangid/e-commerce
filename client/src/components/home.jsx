74258330ab75e96045539bf79a91e24ed1153a60
Vikas-jangid <vikas.jangid@infobeans.com>
2022-12-12 11:49:43 +0530

    added news letter on home page and corrected some para and image positions

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Carousel from "./carousel";
 import sideBanner from "../assests/images/sideBanner.png"
 import Search from "./search";
+import NewsLetter from "./newsLetter";
 
 function Home() {
     return(
@@ -9,18 +10,22 @@ function Home() {
         <Search />
 
         <Carousel/>
-        <div className="grid grid-cols-3 gap-4">
-        <div class="col-span-2 ...">
-            <h1 className="text-center p-2 font-weight-4">Heading 1</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Amet dolorum soluta magni odit optio. Quam numquam dolorum, 
-                libero minima non sunt molestias ipsam perspiciatis architecto. 
-                Nulla hic magni laudantium dolorem?</p>
+        <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols">
+                    <p className="font-mono text-xl text-center leading-loose  p-9">
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. 
+                        It has roots in a piece of classical Latin literature from 45 BC, 
+                        making it over 2000 years old. Richard McClintock, a Latin professor 
+                        at Hampden-Sydney College in Virginia, looked up one of the more obscure 
+                        Latin words, consectetur, from a Lorem Ipsum passage, and going through the
+                        cites of the word in classical literature, discovered the undoubtable source. 
+                    </p>
+            </div>
+            <div className="ml-auto mr-auto">
+                <img src={sideBanner} alt="sideBanner" className="animate-pulse"/>
+            </div>
         </div>
-        <div class="...">
-            <img src={sideBanner} alt="sideBanner" />
-        </div>
-        </div>
+        <NewsLetter />
     </div>
     )
 }
