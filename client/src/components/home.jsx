5863363f9b9867d13ec9ec15ecc0368b37dd53a4
Vikas-jangid <vikas.jangid@infobeans.com>
2022-12-12 14:12:04 +0530

    change class to className

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -10,14 +10,14 @@ function Home() {
 
         <Carousel/>
         <div className="grid grid-cols-3 gap-4">
-        <div class="col-span-2 ...">
+        <div className="col-span-2 ...">
             <h1 className="text-center p-2 font-weight-4">Heading 1</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Amet dolorum soluta magni odit optio. Quam numquam dolorum, 
                 libero minima non sunt molestias ipsam perspiciatis architecto. 
                 Nulla hic magni laudantium dolorem?</p>
         </div>
-        <div class="...">
+        <div className="...">
             <img src={sideBanner} alt="sideBanner" />
         </div>
         </div>
